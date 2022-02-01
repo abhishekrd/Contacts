5ab5748b65c556eeba275531e62c0004aa3c138b
unknown <abhishekrdhanke636@gmail.com>
2022-02-01 15:20:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ function App() {
   return (
     <Router>
            <Navbar />
+
            <Routes>
             
            <Route path="/" element={<Home/>}/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import homeicon from '../images/homeicon.png'
 
 const Navbar = () => {
   return (
      <nav className='navbar'>
          <p className='header'>Contacts</p>
          <div className='button-div'>
+          <Link to="/" ><img className='homeicon' style={{width:30,marginTop:15}} src={homeicon} /> </Link>
            <Link to="/add">
            <button className='addbtn'>Create Contact</button>
            </Link>
